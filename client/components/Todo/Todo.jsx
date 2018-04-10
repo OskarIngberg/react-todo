78ca902d3c2a3630dd528992deb1f103be8d9b60
Oskar Ingberg <oskar.ingberg@claremont.se>
2018-04-10 11:46:36 +0200

    Fix checking problems, adding update functionallity

--- client/components/Todo/Todo.jsx
+++ client/components/Todo/Todo.jsx
@@ -8,26 +8,23 @@ export class Todo extends Component {
         super(props);
 
         this.state = {
+            changeTitle: false,
             id: this.props.id,
             title: this.props.title,
             tasks: this.props.tasks
         }
 
-        this.handleChecking = this.handleChecking.bind(this);
         this.removeTodo = this.removeTodo.bind(this);
         this.renderTasks = this.renderTasks.bind(this);
         this.taskDone = this.taskDone.bind(this);
+        this.checkTaskStatus = this.checkTaskStatus.bind(this);
     }
 
-    handleChecking(element, id, done) {
+    checkTaskStatus(done) {
         var circle = 'far fa-circle';
-        var circleChecked = 'far fa-check-circle';
+        var checkedCircle = 'far fa-check-circle';
 
-        if (done) {
-            element.className = circleChecked;
-        } else {
-            element.className = circle;
-        }
+        return done ? checkedCircle : circle;
     }
 
     taskDone(id) {
@@ -45,14 +42,14 @@ export class Todo extends Component {
 
     renderTasks() {
         var taskDone = this.taskDone;
-        var handleChecking = this.handleChecking;
+        var checkTaskStatus = this.checkTaskStatus;
 
         var tasks = this.state.tasks.map(function(task, i) {
             return (
                 <div key={task._id} className="todo-text">
                     <i 
-                        className={ (element) => handleChecking(task.done) }
-                        onClick = { (event) => taskDone(task._id, task.done) }>
+                        className={ checkTaskStatus(task.done) }
+                        onClick={ (event) => taskDone(task._id) }>
                     </i>
                     <p>{ task.task }</p>
                 </div>
@@ -60,6 +57,37 @@ export class Todo extends Component {
         });
 
         return tasks;
+    }
+
+    title(changeTitle) {
+        if (changeTitle) {
+            return (
+                <input 
+                    type="text"
+                    value={ this.state.title }
+                    onChange={ this.updateTitle.bind(this) }
+                    onKeyPress={ event =>  this.updateTitleDone(event) } 
+                />
+            )
+        } else {
+            return (
+                <h2 
+                    className="todo-title"
+                    onClick={ () => this.setState({ changeTitle: !this.state.changeTitle }) }>
+                        { this.state.title }
+                </h2>
+            )
+        }
+    }
+
+    updateTitle(event) {
+        this.setState({title: event.target.value})
+    }
+
+    updateTitleDone(event) {
+        if (event.key === 'Enter') {
+            this.setState({ changeTitle: !this.state.changeTitle });
+        }
     }
 
     removeTodo(id) {
@@ -74,7 +102,9 @@ export class Todo extends Component {
                     className="fas fa-times remove"
                     onClick = { (event) => this.removeTodo(this.state.id) }>
                 </i>
-                <h2 className="todo-title">{ this.props.title }</h2>
+                {
+                    this.title(this.state.changeTitle)
+                }
                 {
                     this.renderTasks()
                 }
